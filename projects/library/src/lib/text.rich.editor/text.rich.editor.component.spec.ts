9c54012d16161f6b170ff89726d752486e621dbe
Gleb <gleb216@gmail.com>
2024-05-13 02:26:13 +0300

    clear code

--- projects/library/src/lib/text.rich.editor/text.rich.editor.component.spec.ts
+++ projects/library/src/lib/text.rich.editor/text.rich.editor.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TextEditorComponent } from './text.rich.editor.component';
+import { TextRichEditorComponent } from './text.rich.editor.component';
 
 describe('TextEditorComponent', () => {
-  let component: TextEditorComponent;
-  let fixture: ComponentFixture<TextEditorComponent>;
+  let component: TextRichEditorComponent;
+  let fixture: ComponentFixture<TextRichEditorComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TextEditorComponent]
+      imports: [TextRichEditorComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(TextEditorComponent);
+    fixture = TestBed.createComponent(TextRichEditorComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
