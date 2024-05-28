675141b39a772d2dc2385dc79a1870f92c1ae325
Gleb <gleb216@gmail.com>
2024-05-28 22:40:58 +0300

    0.0.3  update to angular 18

--- projects/library/src/lib/module/default.icons/default.icons.module.ts
+++ projects/library/src/lib/module/default.icons/default.icons.module.ts
@@ -2,15 +2,10 @@ import {Inject, NgModule, PLATFORM_ID} from '@angular/core';
 import {DomSanitizer} from '@angular/platform-browser';
 import {MatIconRegistry} from '@angular/material/icon';
 import {isPlatformServer} from '@angular/common';
-import {HttpClientModule} from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import {defaultIcons}  from './default.icons'
 
-@NgModule({
-  declarations: [],
-  imports: [
-    HttpClientModule
-  ]
-})
+@NgModule({ declarations: [], imports: [], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class DefaultIconsModule {
   constructor(private domSanitizer: DomSanitizer, private matIconRegistry: MatIconRegistry, @Inject(PLATFORM_ID) private platformId: string) {
     for (const [name, path] of Object.entries(defaultIcons)) {
--- projects/test.application/src/app/app.config.ts
+++ projects/test.application/src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig } from '@angular/core';
 import {provideAnimations} from '@angular/platform-browser/animations';
-import {provideHttpClient, withFetch} from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
